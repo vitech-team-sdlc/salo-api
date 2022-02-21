bf45a44cd8cbb6bfe98b6a1b6c563556b48e054c
Volodymyr Kvych <v.kvych@gmail.com>
2022-02-21 10:49:56 +0200

    Costs API (EKS and EC2 rates)

--- src/main/java/com/vitechteam/sdlc/cost/model/CostView.java
+++ src/main/java/com/vitechteam/sdlc/cost/model/CostView.java
@@ -1,9 +1,25 @@
 package com.vitechteam.sdlc.cost.model;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 public record CostView(
         BigDecimal maxPerDay,
-        BigDecimal maxPerMonth
+        BigDecimal maxPerMonth,
+        Map<Product, ProductCost> products
 ) {
+    private static final int HOURS_PER_DAY = 24;
+    private static final int DAYS_PER_MONTH = 31;
+
+    public static CostView of(Map<Product, ProductCost> products) {
+        final BigDecimal maxPerDay = products.values().stream()
+                .map(pc -> pc.ratePerHour()
+                        .multiply(BigDecimal.valueOf(pc.quantity()))
+                        .multiply(BigDecimal.valueOf(HOURS_PER_DAY)))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        final BigDecimal maxPerMonth = maxPerDay.multiply(BigDecimal.valueOf(DAYS_PER_MONTH));
+
+        return new CostView(maxPerDay, maxPerMonth, Map.copyOf(products));
+    }
 }
---
+++ src/main/java/com/vitechteam/sdlc/cost/model/Product.java
@@ -0,0 +1,6 @@
+package com.vitechteam.sdlc.cost.model;
+
+public enum Product {
+    AmazonEC2,
+    AmazonEKS
+}
